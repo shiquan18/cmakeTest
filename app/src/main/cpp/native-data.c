9a9d339ab8d910502ec3408554daa919c85ba258
wfq <609790701@qq.com>
2022-07-29 16:24:23 +0800

    main

--- app/src/main/cpp/native-data.c
+++ app/src/main/cpp/native-data.c
@@ -9,7 +9,13 @@
 #define LOGI(...) __android_log_print(ANDROID_LOG_INFO,TAG,__VA_ARGS__)
 #define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG, TAG, __VA_ARGS__)
 #define LOGE(...) __android_log_print(ANDROID_LOG_ERROR,TAG,__VA_ARGS__)
+#define printf(...) __android_log_print(ANDROID_LOG_ERROR,TAG,__VA_ARGS__)
 
+void main();
+
+void test1();
+
+void test2();
 
 JNIEXPORT jstring JNICALL
 Java_com_example_myapplicationcmake_MainActivity_stringFromJNI3(JNIEnv *env, jobject thiz) {
@@ -60,10 +66,69 @@ Java_com_example_myapplicationcmake_MainActivity_accessMethod(JNIEnv *env, jobje
 
 }
 
-JNIEXPORT void JNICALL
+void JNICALL
 Java_com_example_myapplicationcmake_MainActivity_JniMain(JNIEnv *env, jobject thiz) {
-    LOGE("JniMain=%s,", "JniMain start");
-
-
-    LOGE("JniMain=%s,", "JniMain end");
+    LOGE("Main=============================================%s,", "start");
+    main();
+    LOGE("Main=============================================%s,", "end");
 }
+
+void main() {
+    test1();
+//    test2();
+}
+
+void test2() {
+    int num[50], n, *p, j, loop, i, m, k;
+    printf("请输入这一圈人的数量:\n");
+//    scanf("%d", &n);
+    n = 5;
+    p = num;
+    //开始给这些人编号
+    for (j = 0; j < n; j++) {
+        *(p + j) = j + 1;
+    }
+    i = 0;//i用于计数,即让指针后移
+    m = 0;//m记录退出圈子的人数
+    k = 0;//k报数1,2,3
+    while (m < n - 1)//当退出的人数不大于总人数时，即留下的人数至少是一个人
+        //这句不能写成m<n,因为假设有8人，当退出了6人时，此时还是进行人数退出，即m++，
+        //这时是7<8，剩下的一个人自己喊1，2,3那么他也就退出了，将不会有输出
+    {
+        if (*(p + i) != 0)//如果这个人的头上编号不是0就开始报数加1，这里采用的方法是报数为3的人头上编号重置为0
+        {
+            k++;
+        }
+        if (k == 3) {
+            k = 0;    //报数清零，即下一个人从1开始报数
+            *(p + i) = 0;//将报数为3的人编号重置为0
+            m++;    //退出人数加1
+        }
+        i++;      //指针后移
+        if (i == n)//这句很关键，如果到了队尾，就要使指针重新指向对头
+            //并且它只能放在i++后面，因为只有i++了才有可能i==n
+        {
+            i = 0;
+        }
+
+
+    }
+    printf("现在剩下的人是:");
+    for (loop = 0; loop < n; loop++) {
+        if (num[loop] != 0) {
+            printf("%2d号\n", num[loop]);
+        }
+    }
+
+}
+
+void test1() {
+    int a, b;
+    a = 234;
+    b = ~a;
+    printf("a 的按位取反值为（十进制） %d \n", b);
+    a = ~a;
+    printf("a 的按位取反值为（十六进制） %x \n", a);
+}
+
+
